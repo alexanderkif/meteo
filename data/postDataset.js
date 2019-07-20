bc8e4bfb46cf219acc25fe533797eeecd67cb3eb
alexanderkif <alexander_kif@mail.ru>
2019-07-20 07:58:04 +0400

    POST request is checking now if parameters exist. And  can be saved only 4 parameters now

--- data/postDataset.js
+++ data/postDataset.js
@@ -4,8 +4,15 @@ const connectToDatabase = require('./configDb')
 module.exports = async (req, res) => {
    const db = await connectToDatabase(process.env.DB_URI)
    const collection = await db.collection('datasets')
-   let dataset = req.body
+   let dataset = {}
+   dataset.temperature = req.body.temperature
+   dataset.humidity = req.body.humidity
+   dataset.pressure = req.body.pressure
+   dataset.altitude = req.body.altitude
    if (!dataset.created) dataset.created = new Date()
-   const result = await collection.insertOne(dataset)
-   res.status(200).json( result.ops[0] )
+   if (dataset.temperature && dataset.humidity && dataset.pressure) {
+      const result = await collection.insertOne(dataset)
+      res.status(200).json( result.ops[0] )
+   }
+   else res.status(400).send( "Bad Request" )
 }
