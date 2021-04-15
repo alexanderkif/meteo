c4d3403a9d1e029e81677568f5b184a39b7dc8e3
alexanderkif <alexander_kif@mail.ru>
2021-04-15 11:58:06 +0400

    cors

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const connectToDatabase = require('./data/configDb')
 // const microCors = require('micro-cors')
 
 // const cors = microCors({ allowMethods: ['GET','POST'] })
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+// const allowCors = fn => async (req, res) => {
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   )
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end()
+//     return
+//   }
+//   return await fn(req, res)
+// }
 
 const handler = async (req, res) => {
   const db = await connectToDatabase(process.env.DB_URI)
@@ -28,4 +28,5 @@ const handler = async (req, res) => {
 }
 
 // module.exports = cors(handler)
-module.exports = allowCors(handler)
+// module.exports = allowCors(handler)
+module.exports = handler
